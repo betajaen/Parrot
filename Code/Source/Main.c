27ee79c2e408f1707aba91fc4c87b89796d19cce
Robin Southern <betajaen@users.noreply.github.com>
2020-05-06 21:02:02 +0100

    Hello Amiga

---
+++ Code/Source/Main.c
@@ -0,0 +1,36 @@
+/**
+    $Id: Main.c, 0.1, 2020/05/06 20:56:00, betajaen Exp $
+
+    Parrot - Point and Click Adventure Game Player
+    ==============================================
+
+    Copyright 2020 Robin Southern http://github.com/betajaen/parrot
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+#include <exec/types.h>
+
+BYTE VersionString[] = "$VER: Parrot 0.1 (6.5.2020)\r\n";
+BYTE CopyrightString[] = "Copyright(c) 2020 Robin Southern. All Rights Reserved.";
+
+int main()
+{
+  return 0;
+}
