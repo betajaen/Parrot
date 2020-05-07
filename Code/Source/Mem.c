8d3e1f08e75679c2001faba4a1ff5d95728f5b6f
Robin Southern <betajaen@users.noreply.github.com>
2020-05-07 07:55:44 +0100

    Added Mem.c

--- Code/Source/Mem.c
+++ Code/Source/Mem.c
@@ -1,5 +1,5 @@
 /**
-    $Id: Main.c, 0.1, 2020/05/06 20:56:00, betajaen Exp $
+    $Id: Mem.c, 0.1, 2020/05/07 07:29:00, betajaen Exp $
 
     Parrot - Point and Click Adventure Game Player
     ==============================================
@@ -27,14 +27,66 @@
 
 #include "Common.h"
 
-BYTE VersionString[] = "$VER: Parrot 0.1 (6.5.2020)\r\n";
-BYTE CopyrightString[] = "Copyright(c) 2020 Robin Southern. All Rights Reserved.";
+#include <proto/exec.h>
 
-struct ExecBase* SysBase;
+#define MEM_MAGIC 0X944825DFUL
 
-INT main()
+struct MEM_HEADER
 {
-  SysBase = *(struct ExecBase**) 4L;
+  ULONG   mh_Size;
+  ULONG   mh_Magic;
+};
 
-  return 0;
+APTR MemNew(ULONG size, ULONG requirements)
+{
+  struct MEM_HEADER* hdr;
+
+  /* Round up to nearest 4th byte */
+  size = (size + 3) & ~0x03;
+
+  hdr = (struct MEM_HEADER*) AllocVec(size + sizeof(struct MEM_HEADER),
+                                      requirements);
+
+  if (NULL == hdr)
+    return NULL;
+
+  hdr->mh_Size  = size;
+  hdr->mh_Magic = MEM_MAGIC;
+
+  return (APTR)(hdr + 1);
+}
+
+BOOL MemDelete(APTR pMem)
+{
+  struct MEM_HEADER* hdr;
+
+  if (NULL == pMem)
+    return FALSE;
+
+  hdr = ((struct MEM_HEADER*) pMem) - 1;
+
+  if (MEM_MAGIC != hdr->mh_Magic)
+    return FALSE;
+
+  hdr->mh_Magic = 0;
+  hdr->mh_Size = 0;
+  
+  FreeVec(hdr);
+
+  return TRUE;
+}
+
+ULONG MemSize(APTR pMem)
+{
+  struct MEM_HEADER* hdr;
+
+  if (NULL == pMem)
+    return 0UL;
+
+  hdr = ((struct MEM_HEADER*) pMem) - 1;
+
+  if (MEM_MAGIC != hdr->mh_Magic)
+    return 0UL;
+
+  return hdr->mh_Size;
 }
