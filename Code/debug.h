6b13cf7766433d44d5b22a8f003e5480a36fcd1e
Robin Southern <betajaen@users.noreply.github.com>
2024-12-03 15:01:37 +0000

    Added ERROR macro

--- Code/debug.h
+++ Code/debug.h
@@ -3,8 +3,18 @@
 
 #include <exec/types.h>
 
+#define U_STR2(X) #X
+#define U_STR(X) U_STR2(X)
+
 LONG u_Requester(CONST_STRPTR options, CONST_STRPTR text);
 LONG u_RequesterFormat(CONST_STRPTR options, CONST_STRPTR fmt, ...);
+
+#if defined(PARROT_DEBUG) && PARROT_DEBUG==1
+#define ERROR(MSG) \
+    u_Requester("OK", MSG "\n" __FILE__ ":" U_STR(__LINE__) "")
+#else
+#define ERROR(MSG)
+#endif
 
 #endif
 
