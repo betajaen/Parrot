7d2eeaf5f8a3ad81bfc61a3fb533109f5adfabd9
Robin Southern <betajaen@users.noreply.github.com>
2024-12-07 09:59:42 +0000

    EGA pens applied to screen on start

--- Code/main.c
+++ Code/main.c
@@ -3,18 +3,23 @@
 
 #include "gadgets.h"
 #include "palette.h"
+#include "gm_pens.h"
+
 
 static struct Palette* s_palette = NULL;
 static struct GadgetDesc s_gadgets[];
 
-static struct Palette* s_create_ega_palette();
 
 int main(int argc, char** argv) {
 
-    s_palette = s_create_ega_palette();
-    struct Screen* screen = g_OpenScreen(320, 200, 5, "Editor", s_palette);
+    s_palette = g_CreatePalette(20);
+    gm_WritePensToPalette(s_palette);
+
+    struct Screen* screen = g_OpenScreen(320, 200, 4, "Editor");
     if (screen == NULL)
         goto exit;
+
+    g_CopyToScreenPalette(screen, s_palette, 0, 0, 20);
 
     struct Window* window = g_OpenWindow(screen, 0,11,320,200, "Window", WK_Background);
     if (window == NULL)
@@ -28,7 +33,7 @@ int main(int argc, char** argv) {
 
 exit:
     if (s_palette)
-    	g_destroy_palette(s_palette);
+    	g_DestroyPalette(s_palette);
     if (window)
     	g_CloseWindow(window);
     if (screen)
@@ -37,24 +42,10 @@ exit:
     return RETURN_OK;
 }
 
-static struct Palette* s_create_ega_palette() {
-    struct Palette* pal;
-
-    pal = g_create_palette();
-    g_init_palette(pal, 32);
-
-    g_set_palette_colour(pal, 0, 0x00, 0x00, 0x00);
-    g_set_palette_colour(pal, 1, 0xFF, 0xFF, 0xFF);
-    g_set_palette_colour(pal,17, 0xFF, 0xFF, 0xFF);
-
-
-    return pal;
-}
-
 static struct GadgetDesc s_gadgets[] = {
-    G_BUTTON(1, 10,10, 50,15, "Hello"),
+    G_BUTTON(1,  10,10,  50, 15, "Hello"),
     G_BUTTON(2, 130,50, 150,100, "Bonjour"),
 
-    G_PALETTE(3, 10,50, 100,100, 0, 32),
+    G_PALETTE(3, 10,50, 100,100, 0, 16),
     G_STOP
 };
