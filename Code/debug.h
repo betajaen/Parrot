ae55676f7a66c861c41be63b1c3e2b21b71483a1
Robin Southern <betajaen@users.noreply.github.com>
2024-12-07 17:51:06 +0000

    Added U_DEV_ERROR macro

--- Code/debug.h
+++ Code/debug.h
@@ -10,14 +10,19 @@ STRPTR u_Tag2Str(ULONG tag);
 LONG u_Requester(CONST_STRPTR options, CONST_STRPTR text);
 LONG u_RequesterFormat(CONST_STRPTR options, CONST_STRPTR fmt, ...);
 
-#if defined(PARROT_DEBUG) && PARROT_DEBUG==1
 #define U_ERROR(MSG) \
     u_Requester("OK", MSG "\n" __FILE__ ":" U_STR(__LINE__) "")
 #define U_ERROR_FORMAT(FMT, ...) \
     u_RequesterFormat("OK", FMT "\n" __FILE__ ":" U_STR(__LINE__) "", __VA_ARGS__)
+
+#if defined(PARROT_DEBUG) && PARROT_DEBUG==1
+#define U_DEV_ERROR(MSG) \
+    u_Requester("OK", MSG "\n" __FILE__ ":" U_STR(__LINE__) "")
+#define U_DEV_ERROR_FORMAT(FMT, ...) \
+    u_RequesterFormat("OK", FMT "\n" __FILE__ ":" U_STR(__LINE__) "", __VA_ARGS__)
 #else                                                                             
-#define U_ERROR(MSG)
-#define U_ERROR_FORMAT(FMT, ...)
+#define U_DEV_ERROR(MSG)
+#define U_DEV_ERROR_FORMAT(FMT, ...)
 #endif
 
 #endif
